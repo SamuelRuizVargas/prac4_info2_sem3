56e240a61729643236ffe310216957714833c7b9
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-01 13:31:48 -0500

    Aclaracion sobre la forma de los arreglos

--- routers.h
+++ routers.h
@@ -5,8 +5,8 @@ class routers
 {
 private:
     char nombre;
-    char conexiones[7]={0,0,0,0,0,0,0};
-    int precios[7]={-1,-1,-1,-1,-1,-1,-1};
+    char conexiones[7]={0,0,0,0,0,0,0}; //un char 0 simboliza que el router no esta conectado a el router en esa posicion
+    int precios[7]={-1,-1,-1,-1,-1,-1,-1}; // -1 simboliza que no estan conectados, cualquier valor mayor sera tomado como el costo de envio
 
 public:
     routers();
