32db65b85cdc579f7b5afe6e286a80fb29897b62
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-01 13:32:11 -0500

    Formalizacion y analisis del problema y la solucion

--- main.cpp
+++ main.cpp
@@ -1,11 +1,119 @@
 #include <iostream>
+#include <routers.h>
 
 using namespace std;
 
 int main()
 {
     /*
-     * revisar las condiciones para el programa y cuales restricciones son innecesarias
+     * Recopilacion: Un maximo de 7 routers, cada ruter es capaz de conocer los costos de envio hacia
+     * los otros routers a los que esta conectado, se deben poder actualizar los precios y
+     * conexiones en tiempo de ejecucion, se deben poder agregar y eliminar routers.
+     * Debe haber una tabla general de costes de envio para usos internos.
+     * Se debe poder cargar una topologia desde un archivo de texto.
+     * Por ultimo se debe ofrecer al usuario el coste de envio desde un origen a un destino
+     * y ademas indicarle cual es el camino mas corto/eficiente
+     *
+     * Analisis: El programa debe ser extremada mende versatil, que sea capaz de adaptarse a practicamente
+     * cualquier topologia ingresada por el usuario dentro de las regulaciones del mismo, el uso de objetos
+     * y librerias sera escencial en muchos trayectos. Saber que tipo de dato usar sera escencial para que
+     * el programa se ejecute sin problemas.
      */
 
+    int opcion;
+    cout << "Practica 4 - Samuel Ruiz Vargas - CC: 1000898936"<<endl<<endl;
+    cout << "NOTA(S): Una vez cerrado el programa se borrara la red que haya sido creada en el proceso"<<endl<<endl;
+
+    bool terminar=false;
+    int term;
+    while (terminar==false)
+    {
+        cout<<"1 - Inicializar un red por medio de un archivo de texto"<<endl;
+        cout<<"2 - Agregar/Eliminar un enrutador o conexion (maximo de enrutadores: 7)"<<endl;
+        cout<<"3 - Consultar costo de envio y ruta mas eficiente entre enrutadores"<<endl;
+        cout<<"4 - Salir del programa"<<endl<<endl;
+        cout<<"Ingrese la opcion deseada: ";cin>>opcion;
+        while (opcion<1 or opcion>4)
+        {
+            cout<<"Opcion invalida."<<endl;
+            cout<<"Ingrese la opcion deseada: ";cin>>opcion;
+        }
+
+        switch(opcion)
+        {
+            //------------------------------------------------------------------------------------------------------------------
+            case 1: //Inicializar un red por medio de un archivo de texto
+            {
+                cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                while (term<1 or term>2)
+                {
+                    cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                }
+                if(term==1)
+                {
+                    terminar=true;
+                }
+                else
+                {
+                    terminar=false;
+                    for (int i=0;i<=50;i++)
+                    {
+                        cout<<endl;
+                    }
+                }
+            }
+            break;
+            //------------------------------------------------------------------------------------------------------------------
+            case 2: //Agregar/Eliminar un enrutador o conexion
+            {
+                cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                while (term<1 or term>2)
+                {
+                    cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                }
+                if(term==1)
+                {
+                    terminar=true;
+                }
+                else
+                {
+                    terminar=false;
+                    for (int i=0;i<=50;i++)
+                    {
+                        cout<<endl;
+                    }
+                }
+            }
+            break;
+            //------------------------------------------------------------------------------------------------------------------
+            case 3: //Consultar costo de envio y ruta mas eficiente entre enrutadores
+            {
+                cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                while (term<1 or term>2)
+                {
+                    cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
+                }
+                if(term==1)
+                {
+                    terminar=true;
+                }
+                else
+                {
+                    terminar=false;
+                    for (int i=0;i<=50;i++)
+                    {
+                        cout<<endl;
+                    }
+                }
+            }
+            break;
+            //------------------------------------------------------------------------------------------------------------------
+            case 4: //Salir del programa
+            {
+                terminar=true;
+            }
+            break;
+
+        }
+    }
 }
