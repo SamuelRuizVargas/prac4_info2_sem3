e8bd20f1a978975b7a2985a4a22134957c52835e
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-12 14:05:35 -0500

    avances en el codigo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <routers.h>
-#include <funciones.h>
+#include <red.h>
 #include <string.h>
 
 using namespace std;
@@ -34,7 +34,7 @@ int main()
         cout<<"2 - Crear red agregando o eliminando enrutadores o conexiones (maximo de 7 enrutadores)"<<endl;
         cout<<"3 - Salir del programa"<<endl<<endl;
         cout<<"Ingrese la opcion deseada: ";cin>>opcion;
-        while (opcion<1 or opcion>4)
+        while (opcion<1 or opcion>3)
         {
             cout<<"Opcion invalida."<<endl;
             cout<<"Ingrese la opcion deseada: ";cin>>opcion;
@@ -45,31 +45,57 @@ int main()
             //------------------------------------------------------------------------------------------------------------------
             case 1: //Inicializar un red por medio de un archivo de texto
             {
+                for (int i=0;i<=50;i++)
+                {
+                    cout<<endl;
+                }
+
                 string nom_arch, info;
-                int num_rout;
                 cout<<"Ingrese el nombre del archivo en el que se encuentra la topologia(sin extension de archivo): ";cin>>nom_arch;
                 nom_arch+=".txt";//Se le agrega la extension
-                info = txt_copy(nom_arch);
-                num_rout = cant_routers(info);
-//                crear_usu(info, num_rout);
-
-
-                //Codigo para volver al menu principal
-                cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
-                while (term<1 or term>2)
+                red topologia;
+                info = topologia.txt_copy(nom_arch);//se guarda en info los datos que estaban en el txt
+                topologia.crear_routers(info);//se crean los routers usando el metodo de la clase red
+                bool preguntar=true;
+                while (preguntar==true)
                 {
-                    cout<<"Ingrese 1 para terminar o 2 para seguir usando el programa: ";cin>>term;
-                }
-                if(term==1)
-                {
-                    terminar=true;
-                }
-                else
-                {
-                    terminar=false;
                     for (int i=0;i<=50;i++)
                     {
                         cout<<endl;
+                    }
+                    int elec;
+                    cout<<"A continuacion indique que desea hacer con la red: "<<endl<<endl;
+                    cout<<"Ingrese 1 para hacer una consulta sobre rutas y costos."<<endl;
+                    cout<<"Ingrese 2 para agregar o eliminar una conexion o enrutador"<<endl<<endl;
+                    cout<<"Ingrese 3 para volver al menu principal"<<endl<<endl;
+                    cout<<"Opcion: ";cin>>elec;
+                    while(elec<1 or elec>3)
+                    {
+                        cout<<"Opcion invalida."<<endl;
+                        cout<<"Ingrese la opcion deseada: ";cin>>elec;
+                    }
+
+                    switch (elec)
+                    {
+                        case 1: //consultas
+                        {
+                            //realizar metodos para consultar costos y rutas
+                        }
+                        break;
+                        case 2: //modificaciones
+                        {
+                            //realizar metodos para hacer modificaciones a la red
+                        }
+                        break;
+                        case 3: //menu principal
+                        {
+                            preguntar=false;
+                            for (int i=0;i<=50;i++)
+                            {
+                                cout<<endl;
+                            }
+                        }
+                        break;
                     }
                 }
             }
