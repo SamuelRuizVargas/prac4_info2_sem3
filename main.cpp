1f817e3aa174fa959bd081f6408b410bcb72b407
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-15 18:32:56 -0500

    Cambios de adaptacion para los nuevos metodos

--- main.cpp
+++ main.cpp
@@ -54,9 +54,14 @@ int main()
                 string nom_arch, info;
                 cout<<"Ingrese el nombre del archivo en el que se encuentra la topologia(sin extension de archivo): ";cin>>nom_arch;
                 nom_arch+=".txt";//Se le agrega la extension
-                red topologia;
+                char name[7], conexiones[7][7];
+                int price[7][7];
+                red topologia= red();
                 info = topologia.txt_copy(nom_arch);//se guarda en info los datos que estaban en el txt
-                topologia.crear_routers(info);//se crean los routers usando el metodo de la clase red
+                topologia.info_routers(info,name,conexiones,price);//se crean los routers usando el metodo de la clase red
+                //HACER BUCLE PARA CREAR LAS INSTANCIAS DE ROUTERS
+                /*CUANDO TODO ESTE LISTO CONTROLAR CUANDO HAY MAS DE 7
+                 * */
                 bool preguntar=true;
                 while (preguntar==true)
                 {
@@ -80,7 +85,6 @@ int main()
                     {
                         case 1: //consultas
                         {
-                            //realizar metodos para consultar costos y rutas
                             for (int i=0;i<=50;i++)
                             {
                                 cout<<endl;
@@ -105,7 +109,6 @@ int main()
                         break;
                         case 2: //modificaciones
                         {
-                            //realizar metodos para hacer modificaciones a la red
                             for (int i=0;i<=50;i++)
                             {
                                 cout<<endl;
@@ -126,11 +129,17 @@ int main()
                                 case 1: //Agregar router
                                 {
                                     //Hacer metodo en la red para crear un router y actualizar
+                                    topologia.modificar(1);
+                                    cout<<"Pulse la tecla 'Enter' para continuar"<<endl;
+                                    cin.get();
                                 }
                                 break; //Eliminar router
                                 case 2:
                                 {
                                     //Hacer metodo en la red para eliminar un router y actualizar
+                                    topologia.modificar(2);
+                                    cout<<"Pulse la tecla 'Enter' para continuar"<<endl;
+                                    cin.get();
                                 }
                                 break;
                                 case 3: //volver
@@ -157,102 +166,6 @@ int main()
             //------------------------------------------------------------------------------------------------------------------
             case 2: //Crear red agregando o eliminando enrutadores o conexiones (maximo de 7 enrutadores)
             {
-                red topologia;
-                bool preguntar=true;
-                while (preguntar==true)
-                {
-                    for (int i=0;i<=50;i++)
-                    {
-                        cout<<endl;
-                    }
-                    int elec;
-                    cout<<"A continuacion indique que desea hacer con la red: "<<endl<<endl;
-                    cout<<"Ingrese 1 para hacer una consulta sobre rutas y costos."<<endl;
-                    cout<<"Ingrese 2 para agregar o eliminar una conexion o enrutador"<<endl<<endl;
-                    cout<<"Ingrese 3 para volver al menu principal"<<endl<<endl;
-                    cout<<"Opcion: ";cin>>elec;
-                    while(elec<1 or elec>3)
-                    {
-                        cout<<"Opcion invalida."<<endl;
-                        cout<<"Ingrese la opcion deseada: ";cin>>elec;
-                    }
-
-                    switch (elec)
-                    {
-                    case 1: //consultas
-                    {
-                        //realizar metodos para consultar costos y rutas
-                        for (int i=0;i<=50;i++)
-                        {
-                            cout<<endl;
-                        }
-                        char ori, dest;
-                        bool funciona;
-                        funciona = topologia.verificar(); //se verifica la validez de la red a este momento
-                        if (funciona==false)
-                        {
-                            cout<<"La red no cumple los requisitos para hacer una consulta, agregue mas routers y conexiones!"<<endl<<endl;
-                            cout<<"Pulse la tecla 'Enter' para continuar"<<endl;
-                            cin.get();
-                        }
-                        else
-                        {
-                            cout<<"Ingrese el enrutador origen: ";cin>>ori;
-                            cout<<"Ingrese el enrutador destino: ";cin>>dest;
-                            cout<<endl;
-                            topologia.ruta_coste(ori,dest);
-                        }
-                    }
-                    break;
-                    case 2: //modificaciones
-                    {
-                        //realizar metodos para hacer modificaciones a la red
-                        for (int i=0;i<=50;i++)
-                        {
-                            cout<<endl;
-                        }
-                        int opci;
-                        cout<<"Ingrese 1 para agregar un enrutador"<<endl;
-                        cout<<"Ingrese 2 para eliminar un enrutador"<<endl;
-                        cout<<"Ingrese 3 para volver"<<endl;
-                        cout<<"Digite su opcion: ";cin>>opci;
-                        while(opci<1 or opci>3)
-                        {
-                            cout<<"Opcion invalida."<<endl;
-                            cout<<"Digite su opcion: ";cin>>opci;
-                        }
-
-                        switch (opci)
-                        {
-                            case 1: //Agregar router
-                            {
-                                //Hacer metodo en la red para crear un router y actualizar
-                            }
-                            break; //Eliminar router
-                            case 2:
-                            {
-                                //Hacer metodo en la red para eliminar un router y actualizar
-                            }
-                            break;
-                            case 3: //volver
-                            {
-                                cout<<"volviendo..."<<endl;
-                            }
-                            break;
-                        }
-                    }
-                        break;
-                        case 3: //menu principal
-                        {
-                            preguntar=false;
-                            for (int i=0;i<=50;i++)
-                            {
-                                cout<<endl;
-                            }
-                        }
-                        break;
-                    }
-                }
             }
             break;
             //------------------------------------------------------------------------------------------------------------------
