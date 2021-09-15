f73289b4f0bffc356228e8466a07640bb2791182
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-15 18:33:27 -0500

    Agregados y finalizados algunos metodos (sin terminar)

--- red.cpp
+++ red.cpp
@@ -1,6 +1,6 @@
 #include "red.h"
 
-red::red()
+red::red()//constructor
 {
 
 }
@@ -27,9 +27,96 @@ string red::txt_copy(string nombre) //Sacar el txt como string
     return textofin;
 }
 
-void red::crear_routers(string)
+bool red::exis_router(char nombre) //verifica si el router existe en la lista de la red
 {
-    //se recibe el texto y se saca la info para la creacion de los routers encontrados en caso de ser menos de 7
+    bool existe=false;
+    char caracter;
+    for(int i=0;i<7;i++)
+    {
+        caracter=enrutadores[i];
+        if(caracter==nombre)
+        {
+            existe=true;
+        }
+    }
+    return existe;
+}
+
+int red::pos_router(char name, char nombres[7]) //entrega la posicion de un char en un arreglo
+{
+    int pos=0;
+    char caracter;
+    for(int i=0;i<7;i++)
+    {
+        caracter=nombres[i];
+        if(caracter==name)
+        {
+            pos=i;
+            return pos;
+        }
+    }
+    return pos;
+}
+
+void red::info_routers(string texto, char nombres[7], char conectados[7][7], int costos[7][7]) //entrega toda la informacion para crear las instancias de routers
+{
+    vector<string> lineas;
+    int longi=texto.length(), posi, posi2, precio;
+    string caracter, linea, linea2,precio_string;
+    char carac,carac2;
+    for (int i=0;i<=longi;i++)//se separa por lineas
+    {
+        caracter = texto[i];
+        if(caracter != "\n" and i!=longi)
+        {
+            linea+=caracter;
+        }
+        else if(i==longi)
+        {
+            lineas.push_back(linea);
+            linea.clear();
+        }
+        else
+        {
+            lineas.push_back(linea);
+            linea.clear();
+        }
+    }
+    bool existe;
+    int contador=-1;
+    for(auto i=lineas.begin();i != lineas.end(); i++)//se sacan los datos
+    {
+        linea=*i;
+        carac=linea[0];
+        existe=exis_router(carac);
+        if(existe==false)
+        {
+            contador++;
+            nombres[contador]=carac;
+            enrutadores[contador]=carac;
+        }
+        carac2=linea[1];
+        existe=exis_router(carac2);
+        if(existe==false)
+        {
+            contador++;
+            nombres[contador]=carac2;
+            enrutadores[contador]=carac2;
+        }
+        posi=pos_router(carac,nombres);
+        posi2=pos_router(carac2,nombres);
+        conectados[posi][posi2]=carac2;
+        longi=linea.length();
+        for(int k=3;k<longi;k++)
+        {
+            precio_string+=linea[k];
+        }
+        precio=atoi(precio_string.c_str());
+        precio_string.clear();
+        costos[posi][posi2]=precio;
+    }
+
+
 }
 
 void red::ruta_coste(char origen, char destino)
@@ -41,4 +128,14 @@ void red::ruta_coste(char origen, char destino)
 bool red::verificar()
 {
     //verifica que la red tenga almenos 2 routers conectados
+}
+
+void red::modificar(int opcion)
+{
+    //se crea un menu para agregar o eliminar routers segun la opcion con que se llama al metodo (se verifica la cantidad de routers previamente)
+}
+
+red::~red()//destructor
+{
+
 }
--- red.h
+++ red.h
@@ -1,7 +1,10 @@
 #ifndef RED_H
 #define RED_H
+#include "routers.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -10,13 +13,15 @@ class red
 private:
     char enrutadores[7]={0,0,0,0,0,0,0};
 
-
 public:
     red();
     string txt_copy(string);
-    void crear_routers(string);
+    bool exis_router(char);
+    int pos_router(char,char[7]);
+    void info_routers(string, char[7], char[7][7], int[7][7]);
     void ruta_coste(char, char);
     bool verificar();
+    void modificar(int);
     ~red();
 
 };
